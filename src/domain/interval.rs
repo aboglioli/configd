621dd3261594a13b72f3bc5b5ec37c84299d8454
Alan Boglioli <alan.boglioli@gmail.com>
2022-06-13 01:29:23 -0300

    refactor: move Interval to own file

---
+++ src/domain/interval.rs
@@ -0,0 +1,54 @@
+use crate::domain::Error;
+
+#[derive(Debug, PartialEq)]
+pub struct Interval {
+    min: Option<f64>,
+    max: Option<f64>,
+}
+
+impl Interval {
+    pub fn new<N, MIN, MAX>(min: MIN, max: MAX) -> Result<Interval, Error>
+    where
+        MIN: Into<Option<N>>,
+        MAX: Into<Option<N>>,
+        N: Into<f64>,
+    {
+        let min = min.into();
+        let max = max.into();
+
+        if min.is_none() && max.is_none() {
+            return Err(Error::Generic);
+        }
+
+        Ok(Interval {
+            min: min.map(|n| n.into()),
+            max: max.map(|n| n.into()),
+        })
+    }
+
+    pub fn min(&self) -> Option<f64> {
+        self.min
+    }
+
+    pub fn max(&self) -> Option<f64> {
+        self.max
+    }
+
+    pub fn validate<N: Into<f64>>(&self, num: N) -> bool {
+        let num = num.into();
+
+        if let Some(min) = self.min {
+            if num < min {
+                return false;
+            }
+        }
+
+        if let Some(max) = self.max {
+            if num > max {
+                return false;
+            }
+        }
+
+        true
+    }
+}
--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -1,9 +1,13 @@
 mod errors;
+mod interval;
+mod json_schema_builder;
 mod prop;
 mod schema;
 mod value;
 
 pub use errors::*;
+pub use interval::*;
+pub use json_schema_builder::*;
 pub use prop::*;
 pub use schema::*;
 pub use value::*;
