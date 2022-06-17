3c1447fae3667006babe8b56b50294d4efc2c0b8
Alan Boglioli <alan.boglioli@gmail.com>
2022-06-17 17:46:29 -0300

    feature: add Tokio and Axum to build an API

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
 mod domain;
 
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router, Server};
+use std::net::SocketAddr;
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello World" }));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
