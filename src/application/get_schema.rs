e75a74ca41b8f50e900528a0e377d96726b43041
Alan Boglioli <alan.boglioli@gmail.com>
2022-06-19 20:38:29 -0300

    refactor: rename GetSchemaById to GetSchema

---
+++ src/application/get_schema.rs
@@ -0,0 +1,46 @@
+use serde::Deserialize;
+use serde_json::Value as JsonValue;
+use std::sync::Arc;
+
+use crate::{
+    application::SchemaDto,
+    domain::{Error, PropConverter, SchemaId, SchemaRepository},
+};
+
+#[derive(Deserialize)]
+pub struct GetSchemaCommand {
+    pub id: String,
+}
+
+pub type GetSchemaResponse = SchemaDto;
+
+pub struct GetSchema {
+    prop_converter: Arc<dyn PropConverter<JsonValue, Error = Error> + Sync + Send>,
+    schema_repository: Arc<dyn SchemaRepository + Sync + Send>,
+}
+
+impl GetSchema {
+    pub fn new(
+        prop_converter: Arc<dyn PropConverter<JsonValue, Error = Error> + Sync + Send>,
+        schema_repository: Arc<dyn SchemaRepository + Sync + Send>,
+    ) -> GetSchema {
+        GetSchema {
+            prop_converter,
+            schema_repository,
+        }
+    }
+
+    pub async fn exec(&self, cmd: GetSchemaCommand) -> Result<GetSchemaResponse, Error> {
+        let schema_id = SchemaId::new(cmd.id)?;
+
+        if let Some(schema) = self.schema_repository.find_by_id(&schema_id).await? {
+            return Ok(GetSchemaResponse {
+                id: schema.id().to_string(),
+                name: schema.name().to_string(),
+                schema: self.prop_converter.to(schema.into_root_prop())?,
+            });
+        }
+
+        Err(Error::Generic)
+    }
+}
