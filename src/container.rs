b3609703b85218ae6429c670f9b991562819eaae
Alan Boglioli <alan.boglioli@gmail.com>
2022-07-04 01:04:17 -0300

    feature: add Sha256Hasher dependency to Container

--- src/container.rs
+++ src/container.rs
@@ -4,23 +4,28 @@ use std::sync::Arc;
 use crate::{
     config::{Config, Storage},
     domain::Error,
-    infrastructure::InMemSchemaRepository,
+    infrastructure::{InMemSchemaRepository, Sha256Hasher},
 };
 
 pub struct Container {
     pub event_publisher: Arc<LocalEventBus>,
+    pub hasher: Arc<Sha256Hasher>,
     pub schema_repository: Arc<InMemSchemaRepository>,
 }
 
 impl Container {
     pub fn build(config: &Config) -> Result<Container, Error> {
         let event_publisher = Arc::new(LocalEventBus::new());
+
+        let hasher = Arc::new(Sha256Hasher::new());
+
         let schema_repository = Arc::new(match config.storage {
             Storage::InMem => InMemSchemaRepository::new(),
         });
 
         Ok(Container {
             event_publisher,
+            hasher,
             schema_repository,
         })
     }
