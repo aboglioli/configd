77b6cd68f9c903dd377ad2bdd1ad0ef6a8007665
Alan Boglioli <alan.boglioli@gmail.com>
2022-07-05 01:28:05 -0300

    feature: ESLint + Prettier + Editorconfig for linting and code style

---
+++ web/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'comma-dangle': ['error', 'always-multiline'],
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'prettier/prettier': 'error',
+    quotes: ['error', 'single'],
+    'react/react-in-jsx-scope': 'off',
+    semi: ['error', 'always'],
+  },
+};
